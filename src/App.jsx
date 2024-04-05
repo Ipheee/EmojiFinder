530cbf89587235e15211ccd5625961ecb0a61c21
Ipheee <132576780+Ipheee@users.noreply.github.com>
2024-04-05 01:25:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
-import { Emoji } from './components/Emoji'
+import { Emoji } from './components/Emoji';
 
 function App() {
   
   return(
-    <div>
+    <div className='emojiApp'>
+      <div className='container'>
       <Emoji/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+
--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -1,56 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {FaSearch} from 'react-icons/fa';
+import './Emoji.css';
 
 export const Emoji = () => {
-    const [emojis, setEmojis] = useState([])
-    const [value, setValue] = useState(null)
-    const [searchResult, setSearchResult] = useState([])
+  const [emojis, setEmojis] = useState('');
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState([]);
+  
 
 
-    useEffect(() => {
-        const fetchEmoji = async () =>{
-            try{
-                    
-                        const response = await axios.get('https://emoji-api.com/emojis?access_key=b4678fd1781cfcd57f01ffafca680c0378c4b7bd')
-                        setEmojis(response.data)
-                    
-                }
-        
-                catch (error){
-                    console.error("Unable to get data:", error.message)
-        
-                }
-            
-        }
-
-        fetchEmoji()
-    })
-
-    const handleChange =() =>{
-        e.preventDefault()
-        // const result = emojis.find((emoji => emoji))
-        // const searchTerm = (event) => {
-        //     const filteredEmoji = object.keys(emoji).filter((emoji) =>
-        //     )
+  useEffect(() => {
+    const fetchData = async (value) =>{
+      try{
+                      
+        const response = await axios.get('https://emoji-api.com/emojis',{
+          params: {
+            access_key: 'b4678fd1781cfcd57f01ffafca680c0378c4b7bd',
+            search: value,
+          },      
+                  
+        });
+  
+        // console.log(response.data);
+        setEmojis(response.data);
+  
+        // if (response.data.length === 1) {
+        //   setEmojis('single');
+        // } else {
+        //   setEmojis('');
         // }
+        
+        // if (handleCopy (results.character == 1))
+        // alert ('copied');
+  
+          
+      } catch (error){
+        console.error("Unable to get data:", error.message);    
+      }  
+    };
+  
+    fetchData()
+  },[])
 
+  console.log(input)
+    const handleCopy = (character) => {
+      navigator.clipboard.writeText(character);
+      alert ('copied');
+      
+    };
+
+    // if (handleCopy (results.character == 1))
+    //     alert ('copied');
+
+    const handleSearch = (e) => {
+      e.preventDefault()
+      const searchedEmojis = emojis.filter((emoji) => emoji.slug.includes(input))
+      setResults(searchedEmojis)
     }
     
-    
+    // console.log(results)
+
   return (
-    <main>
-        <form className="formInput" onChange={handleChange}>
-            <input type ="text" placeholder="Search emoji..." id="emojiText"onChange={(e) => setValue(e.target.value)}/>
-            <button>Search</button>
-
+    <div>
+        <form>
+            <input className='emoji'
+            placeholder="Search emoji..." id="emoji" 
+            value={input}
+            onChange={(e) => setInput(e.target.value)}/>
+          <button onClick={handleSearch}><FaSearch id='searchIcon'/></button>
+          
+            
+            
         </form>
-        {
-            emojis.map((emoji, index) =>(
-                <span key={index}> {emoji.character}</span>
-            ))
-
-        }
         
-        </main>
-  )
-}
+        <div className='emojiSection'>
+        {emojis.length > 0 &&
+          results.map((result, index) => (
+              <span className={'icon ' + result} key={index}onClick={() => handleCopy(result.character)} title={result.slug}>{result.character}</span>
+
+              
+            
+          ))}
+        </div>
+        </div>
+  );
+};
